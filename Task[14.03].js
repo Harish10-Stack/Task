3cd45cc1bbe73df4c3084bae820e052f340000ca
Harish10-Stack <harishkaladharan10@gmail.com>
2025-03-17 11:27:01 +0530

    Add files via upload

---
+++ Task[14.03].js
@@ -0,0 +1,26 @@
+// Write an if statement that checks if a number is possitive.
+let num1 = 4
+
+if (num1>0) {
+    console.log("Num1 is Posstive");
+}else{
+    console.log("Num1 is not Possitive");
+}
+
+// Write an if statement that checks if a number is even.
+let num2 = 8
+
+if (num2 %2 == 0) {
+    console.log("Num2 is Even");
+}else{
+    console.log("Num2 is not Even");
+}
+
+// Write a function to return the square of a number.
+function square(num) {
+    return num * num;
+}
+console.log(square("6"));
+
+
+
